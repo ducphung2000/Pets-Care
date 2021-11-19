9c064d1b0d5a3f0655be9431c477547c687f5d0a
Đức Phụng <ducphung2k@gmail.com>
2021-11-19 21:38:58 +0700

    19/11

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pets Care</title>
     <?php include("./public/layout/head.php");?>
+    
     <link rel="stylesheet" href="./public/csslayout/home.css">
 </head>
 <body>
